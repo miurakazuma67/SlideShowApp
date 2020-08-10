6ff694e9a277d0e83692ac70ce8c65cc58ecf8d2
miurakazuma67 <miurakazuma67@gmail.com>
2020-08-10 13:13:32 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by 三浦　一真 on 2020/08/10.
+//  Copyright © 2020 Kazuma Miura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
